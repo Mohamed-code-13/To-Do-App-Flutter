203cc3a85e5863f287b5ba62a82af77c841241ae
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-09-25 19:25:11 +0300

    Created no tasks screen.

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:to_do_app/presentation/size_config/size_config.dart';
 
 import '../../logic/theme_cubit/theme_cubit.dart';
 import '../widgets/custom_button.dart';
@@ -30,13 +32,42 @@ class HomeScreen extends StatelessWidget {
       ),
       body: SafeArea(
         child: Center(
-          child: CustomButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(AddTaskScreen.routeName);
-              },
-              title: '+ Add Task'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(AddTaskScreen.routeName);
+                  },
+                  title: '+ Add Task'),
+              _noTasks(context),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _noTasks(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset(
+          'assets/images/task.png',
+          color: Theme.of(context).colorScheme.primary,
+          width: SizeConfig.screenWidth / 2.5,
+        ),
+        Text(
+          'You don\'t have any tasks yet',
+          textAlign: TextAlign.center,
+          style: GoogleFonts.robotoMono(
+            fontSize: SizeConfig.getProportionateScreenWidth(18),
+            color: BlocProvider.of<ThemeCubit>(context).themeMode ==
+                    ThemeMode.light
+                ? Theme.of(context).colorScheme.primary
+                : Colors.grey,
+          ),
+        ),
+      ],
     );
   }
 }
