faa33894ab3c71831c2102060f539d636d9b821d
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-09-27 18:22:43 +0300

    Create type adapter for task model.

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,11 +1,24 @@
+import 'package:hive/hive.dart';
+
+part 'task_model.g.dart';
+
+@HiveType(typeId: 0)
 class TaskModel {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String description;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final String startTime;
+  @HiveField(4)
   final String endTime;
+  @HiveField(5)
   final String repeat;
+  @HiveField(6)
   final bool isCompleted;
+  @HiveField(7)
   final int color;
 
   const TaskModel({
---
+++ lib/models/task_model.g.dart
@@ -0,0 +1,62 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TaskModelAdapter extends TypeAdapter<TaskModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  TaskModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return TaskModel(
+      title: fields[0] as String,
+      description: fields[1] as String,
+      date: fields[2] as String,
+      startTime: fields[3] as String,
+      endTime: fields[4] as String,
+      repeat: fields[5] as String,
+      isCompleted: fields[6] as bool,
+      color: fields[7] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, TaskModel obj) {
+    writer
+      ..writeByte(8)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.description)
+      ..writeByte(2)
+      ..write(obj.date)
+      ..writeByte(3)
+      ..write(obj.startTime)
+      ..writeByte(4)
+      ..write(obj.endTime)
+      ..writeByte(5)
+      ..write(obj.repeat)
+      ..writeByte(6)
+      ..write(obj.isCompleted)
+      ..writeByte(7)
+      ..write(obj.color);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
