3ab661a89739ede78d02ed2136985f60cd7a52db
Mohamed Ashraf <mohamed.code.13@gmail.com>
2023-09-27 21:14:34 +0300

    Create animation to the tasks list

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'logic/add_task_cubit/add_task_cubit.dart';
-import 'logic/read_task_cubit/read_task_cubit.dart';
 import 'presentation/screens/add_task_screen.dart';
 import 'presentation/screens/home_screen.dart';
 
--- lib/presentation/widgets/tasks_list.dart
+++ lib/presentation/widgets/tasks_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:to_do_app/presentation/widgets/task_tile.dart';
 
 import '../../models/task_model.dart';
@@ -13,7 +14,18 @@ class TasksList extends StatelessWidget {
     return ListView.builder(
       itemCount: tasks.length,
       physics: const BouncingScrollPhysics(),
-      itemBuilder: (ctx, index) => TaskTile(task: tasks[index]),
+      itemBuilder: (ctx, index) {
+        return AnimationConfiguration.staggeredList(
+          duration: const Duration(milliseconds: 300),
+          position: index,
+          child: SlideAnimation(
+            horizontalOffset: 300,
+            child: FadeInAnimation(
+              child: TaskTile(task: tasks[index]),
+            ),
+          ),
+        );
+      },
     );
   }
 }
